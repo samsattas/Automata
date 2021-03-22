9d9d94857674b7e8031655d349d02589df26bc0d
samuel satizabal <48489513+samsattas@users.noreply.github.com>
2021-03-22 02:22:22 -0500

    First commit

---
+++ Automata/UI/MainInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automata
+{
+    public partial class MainInterface : Form
+    {
+        public MainInterface()
+        {
+            InitializeComponent();
+            //numericOutputs.Enabled = false;
+            //Console.WriteLine(numericOutputs.Value);
+            textBoxStates.Width = 20 + (30 * (Convert.ToInt32(Math.Round(numericOutputs.Value, 0))));
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            numericOutputs.Enabled = false;
+            textBoxStates.Width = 20 + (30 * (Convert.ToInt32(Math.Round(numericOutputs.Value, 0))));
+        }
+    }
+}
